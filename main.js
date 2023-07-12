2e54429609393b5e15ba462313dcff41feb7c9ae
samuerikcoder <eriksamumedeiros@gmail.com>
2023-07-12 13:33:36 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav-toggle');
+
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+});
